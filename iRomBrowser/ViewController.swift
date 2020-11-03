a7a6783fce329645717b7a3e66c5a30842de4ee9
roman dod <stalkeroma1@mail.ru>
2020-11-03 12:24:43 +0300

    create structure of project and add some functionality

--- iRomBrowser/ViewController.swift
+++ iRomBrowser/ViewController.swift
@@ -7,14 +7,85 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, WKUIDelegate {
+    
+    @IBOutlet weak var bottomMenuOutlet: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var cancelButtonOutlet: UIButton!
+    @IBOutlet weak var shareButtonOutlet: UIButton!
+    @IBOutlet weak var backButtonOutlet: UIButton!
+    @IBOutlet weak var addButtonOutlet: UIButton!
+    @IBOutlet weak var forwardButtonOutlet: UIButton!
+    @IBOutlet weak var goButtonOutlet: UIButton!
+    @IBOutlet weak var wkWebViewOutlet: UIView!
+    
+//    var isLoading: Bool = false
+    var webView: WKWebView!
+    var url: String = "https://www.instagram.com/roma_dod.io"
+    
+    
+    override func loadView() {
+//        let webConfiguration = WKWebViewConfiguration()
+//        webView = WKWebView(frame: wkWebViewOutlet.frame, configuration: webConfiguration)
+//        webView.uiDelegate = self
+//        wkWebViewOutlet = webView
+//        wkWebViewOutlet.addSubview(webView)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        webView = WKWebView(frame: wkWebViewOutlet.bounds, configuration: WKWebViewConfiguration())
+        wkWebViewOutlet.addSubview(webView)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let request = URLRequest(url: URL(string: url)!)
+        webView.load(request)
+//        let wkWebView = WKWebView(frame: wkWebViewOutlet.frame)
+//
+//        self.wkWebViewOutlet.addSubview(wkWebView)
+//
+//        guard let url = URL(string: url) else { return }
+//        let request = URLRequest(url: url)
+//        wkWebView.load(request)
     }
 
-
+    @IBAction func pressedCancelButton(_ sender: UIButton) {
+        if webView.isLoading == true {
+            webView.stopLoading()
+        }
+    }
+    
+    @IBAction func pressedShareButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pressedBackButton(_ sender: UIButton) {
+        if webView.canGoBack == true {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func pressedAddButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pressedForwardButton(_ sender: UIButton) {
+        if webView.canGoForward == true {
+            webView.goForward()
+        }
+    }
+    
+    @IBAction func pressedGoButton(_ sender: UIButton) {
+        
+        let myURL = URL(string: url)
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+    
+    
 }
-
---
+++ iRomBrowser/WebPage/WebPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WebPageViewController.swift
+//  iRomBrowser
+//
+//  Created by Roman Dod on 11/3/20.
+//  Copyright © 2020 Roman Dod. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
